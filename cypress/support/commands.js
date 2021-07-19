be9b7064874443440249b965d2bb70ad4b0fcc01
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2021-07-19 23:07:25 +0300

    Added login.json fixture

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,12 +1,11 @@
 Cypress.Commands.add('login', () => {
+  cy.fixture('login.json').then(login => {
+    cy.request('POST', 'http://localhost:3333/api/users/login', {'email': 'test@test.com', 'password': 'test'}).
+        then(response => {
+          window.localStorage.setItem("userInfo", JSON.stringify(response.body.data))
+        });
+  })
 
-  // TODO: Read data from fixture
-
-  cy.request('POST', 'http://localhost:3333/api/users/login', {'email': 'test@test.com', 'password': 'test'}).
-      then(response => {
-        window.localStorage.setItem("userInfo", JSON.stringify(response.body.data))
-      });
-
-  cy.visit('http://localhost:4200')
+  cy.visit('/')
   cy.get('.navbar .nav-link').contains('test').should('be.visible')
 })
