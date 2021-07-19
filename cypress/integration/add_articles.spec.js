32d569ff38d250785fa9406e97f230e84aecee0d
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2021-07-19 21:21:16 +0300

    Covered test scenarios: 11 articles are created successfully

--- cypress/integration/add_articles.spec.js
+++ cypress/integration/add_articles.spec.js
@@ -2,158 +2,35 @@
 
 describe('user creating articles', () => {
   beforeEach(() => {
-    cy.login()
+    cy.login();
   });
 
   it('can create 11 new articles', () => {
-    cy.get('[href="#/editor"]').click()
+    cy.wrap(Array.from({length: 11}, (v, k) => k + 1)).each(index => {
+      cy.get('[href="#/editor"]').click();
 
-    // TODO: Read article data from fixture file or init it somehow
-    cy.get('[name=title]').should('be.visible').type('Article1')
-    cy.get('[name=description]').type('Description1')
-    cy.get('[name=body]').type('Body1')
-    cy.get('[name=tagList]').type('tag1,tag2')
-    cy.get('[type=submit]').click()
+      // TODO: Read article data from fixture file or init it somehow
+      cy.get('[name=title]').should('be.visible').type('Article #' + index);
+      cy.get('[name=description]').type('Description #' + index);
+      cy.get('[name=body]').type('Body #' + index);
+      cy.get('[name=tagList]').type('tag#' + index + ',tag#' + (index + 1));
+      cy.get('[type=submit]').click();
 
-    cy.contains('Edit Article').should('be.visible')
-    cy.get('.banner h1').should('have.text', 'Article1')
-    cy.get('.article-content > div > div').
-        first().should('have.text', 'Description1').
-        next().should('contain.text', 'Body1')
+      cy.contains('Edit Article').should('be.visible');
+      cy.get('.banner h1').should('have.text', 'Article #' + index);
+      cy.get('.article-content > div > div').
+          first().should('have.text', 'Description #' + index).
+          next().should('contain.text', 'Body #' + index);
 
-    cy.get('.tag-list > .tag-pill')
-    .should('have.length', 2)
-    .then(($els) => {
-      // we get a list of jQuery elements
-      // let's convert the jQuery object into a plain array
-      return (
-          Cypress.$.makeArray($els)
-              // and extract inner text from each
-              .map((el) => el.innerText)
-      )
-    })
-    .should('deep.equal', ['tag1', 'tag2'])
-
-    // cy.get('[name=body]').type('Body1');
-    // cy.get('[name=tagList]').type('tag1,tag2');
-
-    // // We use the `cy.get()` command to get all elements that match the selector.
-    // // Then, we use `should` to assert that there are two matched items,
-    // // which are the two default items.
-    // cy.get('.todo-list li').should('have.length', 2)
-    //
-    // // We can go even further and check that the default todos each contain
-    // // the correct text. We use the `first` and `last` functions
-    // // to get just the first and last matched elements individually,
-    // // and then perform an assertion with `should`.
-    // cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+      cy.get('.tag-list > .tag-pill').should('have.length', 2).then(($els) => {
+        // we get a list of jQuery elements
+        // let's convert the jQuery object into a plain array
+        return (
+            Cypress.$.makeArray($els)
+                // and extract inner text from each
+                .map((el) => el.innerText)
+        );
+      }).should('deep.equal', ['tag#' + index, 'tag#' + (index + 1)]);
+    });
   });
-
-  // it('can add new todo items', () => {
-  //   // We'll store our item text in a variable so we can reuse it
-  //   const newItem = 'Feed the cat'
-  //
-  //   // Let's get the input element and use the `type` command to
-  //   // input our new list item. After typing the content of our item,
-  //   // we need to type the enter key as well in order to submit the input.
-  //   // This input has a data-test attribute so we'll use that to select the
-  //   // element in accordance with best practices:
-  //   // https://on.cypress.io/selecting-elements
-  //   cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
-  //
-  //   // Now that we've typed our new item, let's check that it actually was added to the list.
-  //   // Since it's the newest item, it should exist as the last element in the list.
-  //   // In addition, with the two default items, we should have a total of 3 elements in the list.
-  //   // Since assertions yield the element that was asserted on,
-  //   // we can chain both of these assertions together into a single statement.
-  //   cy.get('.todo-list li')
-  //     .should('have.length', 3)
-  //     .last()
-  //     .should('have.text', newItem)
-  // })
-  //
-  // it('can check off an item as completed', () => {
-  //   // In addition to using the `get` command to get an element by selector,
-  //   // we can also use the `contains` command to get an element by its contents.
-  //   // However, this will yield the <label>, which is lowest-level element that contains the text.
-  //   // In order to check the item, we'll find the <input> element for this <label>
-  //   // by traversing up the dom to the parent element. From there, we can `find`
-  //   // the child checkbox <input> element and use the `check` command to check it.
-  //   cy.contains('Pay electric bill')
-  //     .parent()
-  //     .find('input[type=checkbox]')
-  //     .check()
-  //
-  //   // Now that we've checked the button, we can go ahead and make sure
-  //   // that the list element is now marked as completed.
-  //   // Again we'll use `contains` to find the <label> element and then use the `parents` command
-  //   // to traverse multiple levels up the dom until we find the corresponding <li> element.
-  //   // Once we get that element, we can assert that it has the completed class.
-  //   cy.contains('Pay electric bill')
-  //     .parents('li')
-  //     .should('have.class', 'completed')
-  // })
-  //
-  // context('with a checked task', () => {
-  //   beforeEach(() => {
-  //     // We'll take the command we used above to check off an element
-  //     // Since we want to perform multiple tests that start with checking
-  //     // one element, we put it in the beforeEach hook
-  //     // so that it runs at the start of every test.
-  //     cy.contains('Pay electric bill')
-  //       .parent()
-  //       .find('input[type=checkbox]')
-  //       .check()
-  //   })
-  //
-  //   it('can filter for uncompleted tasks', () => {
-  //     // We'll click on the "active" button in order to
-  //     // display only incomplete items
-  //     cy.contains('Active').click()
-  //
-  //     // After filtering, we can assert that there is only the one
-  //     // incomplete item in the list.
-  //     cy.get('.todo-list li')
-  //       .should('have.length', 1)
-  //       .first()
-  //       .should('have.text', 'Walk the dog')
-  //
-  //     // For good measure, let's also assert that the task we checked off
-  //     // does not exist on the page.
-  //     cy.contains('Pay electric bill').should('not.exist')
-  //   })
-  //
-  //   it('can filter for completed tasks', () => {
-  //     // We can perform similar steps as the test above to ensure
-  //     // that only completed tasks are shown
-  //     cy.contains('Completed').click()
-  //
-  //     cy.get('.todo-list li')
-  //       .should('have.length', 1)
-  //       .first()
-  //       .should('have.text', 'Pay electric bill')
-  //
-  //     cy.contains('Walk the dog').should('not.exist')
-  //   })
-  //
-  //   it('can delete all completed tasks', () => {
-  //     // First, let's click the "Clear completed" button
-  //     // `contains` is actually serving two purposes here.
-  //     // First, it's ensuring that the button exists within the dom.
-  //     // This button only appears when at least one task is checked
-  //     // so this command is implicitly verifying that it does exist.
-  //     // Second, it selects the button so we can click it.
-  //     cy.contains('Clear completed').click()
-  //
-  //     // Then we can make sure that there is only one element
-  //     // in the list and our element does not exist
-  //     cy.get('.todo-list li')
-  //       .should('have.length', 1)
-  //       .should('not.have.text', 'Pay electric bill')
-  //
-  //     // Finally, make sure that the clear button no longer exists.
-  //     cy.contains('Clear completed').should('not.exist')
-  //   })
-  // })
 });
