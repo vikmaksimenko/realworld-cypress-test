b947929431278340bbf7f4eaaa3ce99582ab3353
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2021-07-19 21:07:56 +0300

    Setup a test for adding 1 article

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', () => {
+
+  // TODO: Read data from fixture
+
+  cy.request('POST', 'http://localhost:3333/api/users/login', {'email': 'test@test.com', 'password': 'test'}).
+      then(response => {
+        window.localStorage.setItem("userInfo", JSON.stringify(response.body.data))
+      });
+
+  cy.visit('http://localhost:4200')
+  cy.get('.navbar .nav-link').contains('test').should('be.visible')
+})
